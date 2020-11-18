89376960675c450d51029ddcf7664d4f7c1e7365
Victor Marcolino <marcolino.victor@gmail.com>
2020-11-18 20:34:36 -0300

    check that

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -8,34 +8,37 @@ import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../types";
 import {fetch_devices, toggle_switch} from "../store/actions/devices";
 
-
-export default function HomeScreen() {
+export default function HomeScreen(props: any) {
     const devices = useSelector((state: RootState) => state.devices);
     const config = useSelector((state: RootState) => state.config);
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(fetch_devices());
     }, [dispatch, config.host.url])
+    console.log(props)
     return (
-        <View style={{marginHorizontal: 16}}>
+        <View style={{marginHorizontal: 16, backgroundColor: Colors.dark.text}}>
             <FlatList data={devices} keyExtractor={item => item.identifier}
+                      style={{backgroundColor: Colors.dark.text}}
                       renderItem={
                           ({index, item, separators}) => {
-                              return (
+                              return <View style={{backgroundColor: "#f0f0f0"}}>
                                   <Card onPress={() => {
                                       console.log('pressed');
-                                  }} elevation={0} style={{marginBottom: 10, backgroundColor: Colors.primaryColor}}>
+                                  }} elevation={0} style={{marginBottom: 16, backgroundColor: Colors.primaryColor}}>
                                       <Card.Title title={item.identifier} subtitle={`Ligado: ${item.is_on}`}/>
                                       <Card.Content>
                                           <Text>Device {index}</Text>
                                       </Card.Content>
-                                      <Card.Cover source={{uri: 'https://picsum.photos/700'}}/>
+                                      {/*<Card.Cover source={{uri: 'https://picsum.photos/700'}}/>*/}
                                       <Card.Actions style={styles.container}>
                                           <Button onPress={() => {
                                               dispatch(toggle_switch({...item, is_on: !item.is_on}));
                                           }}>Switch</Button>
                                       </Card.Actions>
-                                  </Card>)
+                                  </Card>
+                              </View>
+
                           }
                       }>
             </FlatList>
